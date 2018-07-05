d617c8d3b21ee9413a6793f973ccd12e8a2240b8
broliverparker <b4009245@my.shu.ac.uk>
2018-07-05 16:23:40 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HDDesicions.Models;
+
+namespace HDDesicions.Controllers
+{
+    public class HomeController : Controller
+    {
+        Applicant applicant = new Applicant();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Apply()
+        {
+            return View(applicant);
+        }
+
+        public ActionResult Result(string fname, string lname, string dob, string income)
+        {
+            applicant.FirstName = fname;
+            applicant.LastName = lname;
+            applicant.DOB = dob;
+            applicant.Income = income;
+
+            return View(applicant);
+        }
+
+        public ActionResult PreviousResult()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Applicant.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HDDesicions.Models
+{
+    public class Applicant
+    {
+        public string FirstName
+        { get; set; }
+
+        public string LastName
+        { get; set; }
+
+        public string DOB
+        { get; set; }
+
+        public string Income
+        { get; set; }
+
+        public int CalculateAge (string applicationDOB)
+        {
+
+            int now = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+            int dob = int.Parse(applicationDOB);
+
+            return int.Parse(((now - dob) / 10000).ToString().Substring(0,1));
+        }
+
+        public int ValidateApplication(string applicationDOB, string income)
+        {
+            if(CalculateAge(applicationDOB) > 18)
+            {
+                if (int.Parse(income) > 30000)
+                    return 1;
+                else
+                    return 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
